b84d8ce06387bb473a90efa58e8204cc6e621736
Tobias <tobias.weinlich@gmx.de>
2023-04-20 23:02:27 +0200

    Added documentation and error handling

--- src/typeScript/classes/character.class.ts
+++ src/typeScript/classes/character.class.ts
@@ -1,8 +1,11 @@
 
+// ----- import interfaces -----
 import { AbilityScores } from '../interfaces/abilityScores.interface';
 import { Skills } from '../interfaces/skills.interface';
-import { loadSkillsTranslations } from '../functions/translations/loadSkillsTranslations.function';
-import { loadAbilityScoresTranslations } from '../functions/translations/loadAbilityScoresTranslations.function';
+
+// ----- import functions -----
+import { loadSkillTranslations } from '../functions/translations/loadSkillTranslations.function';
+import { loadAbilityScoreTranslations } from '../functions/translations/loadAbilityScoreTranslations.function';
 
 
 export class Character {
@@ -13,20 +16,16 @@ export class Character {
         private _skills: Skills,
         private _proficiencyBonus: number,
     ) {
-        this._abilityScores = loadAbilityScoresTranslations(this._language, this._abilityScores);
-        this._skills = loadSkillsTranslations(this._language, this._skills);
+        this._abilityScores = loadAbilityScoreTranslations(this._language, this._abilityScores);
+        this._skills = loadSkillTranslations(this._language, this._skills);
     }
+
+    // ----- getters and setters -----
 
     get language(): string {
         return this._language;
     }
-
-    setLanguage(language: string) {
-        this._language = language;
-        this._abilityScores = loadAbilityScoresTranslations(this._language, this._abilityScores);
-        this._skills = loadSkillsTranslations(this._language, this._skills);
-    }
-
+    
     get name(): string {
         return this._name;
     }
@@ -34,11 +33,11 @@ export class Character {
     set name(name: string) {
         this._name = name;
     }
-
+    
     get abilityScores(): AbilityScores {
         return this._abilityScores;
     }
-
+    
     set abilityScores(abilityScores: AbilityScores) {
         this._abilityScores = abilityScores;
     }
@@ -46,24 +45,56 @@ export class Character {
     get skills(): Skills {
         return this._skills;
     }
-
+    
     set skills(skills: Skills) {
         this._skills = skills;
     }
-
+    
     get proficiencyBonus(): number {
         return this._proficiencyBonus;
     }
-
+    
     set proficiencyBonus(proficiencyBonus: number) {
         this._proficiencyBonus = proficiencyBonus;
     }
+    
+    // ----- public methods -----
 
-    getAbilityScoreModifier(abilityName: keyof AbilityScores): number {
-        const abilityScore = this._abilityScores[abilityName].value;
-        return Math.floor((abilityScore - 10) / 2);
+    /**
+     * Sets the language for the character sheet.
+     * @param language - A string that represents the language in which the character sheet should be displayed.
+     * @enum {string} - Valid languages are: 'en' (English), 'de' (German).
+     * @throws {Error} If an unsupported language is passed as an argument.
+     */
+    setLanguage(language: string) {
+        const validLanguages: string[] = ['en', 'de'];
+
+        if (!validLanguages.includes(language)) {
+            throw new Error(`Unsupported language: ${language}`);
+        }
+
+        this._language = language;
+        this._abilityScores = loadAbilityScoreTranslations(this._language, this._abilityScores);
+        this._skills = loadSkillTranslations(this._language, this._skills);
     }
-
+    
+    /**
+     * Calculates the ability score modifier for a specified ability score.
+     * @param {keyof AbilityScores} abilityName - A string that represents the name of the ability score for which the modifier needs to be calculated.
+     * @enum {string} - Valid ability names are: 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'.
+     * @returns {number} A number that represents the modifier for the specified ability score.
+    */
+   getAbilityScoreModifier(abilityName: keyof AbilityScores): number {
+       const abilityScore = this._abilityScores[abilityName].value;
+       return Math.floor((abilityScore - 10) / 2);
+    }
+    
+    /**
+     * Calculates the modifier for a specified skill based on the character's ability score and proficiency bonus.
+     * @param {keyof Skills} skillName - A string that represents the name of the skill for which the modifier needs to be calculated.
+     * @enum {string} - Valid skill names are: 'acrobatics', 'animalHandling', 'arcana', 'athletics', 'deception', 'history', 'insight', 'intimidation', 'investigation', 'medicine', 'nature', 'perception', 'performance', 'persuasion', 'religion', 'sleightOfHand', 'stealth', 'survival'.
+     * @returns {number} A number that represents the modifier for the specified skill.
+     */
     getSkillModifier(skillName: keyof Skills): number {
         const skill = this._skills[skillName];
         const abilityScoreModifier = this.getAbilityScoreModifier(skill.ability);
